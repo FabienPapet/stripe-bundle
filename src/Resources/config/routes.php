55ded64bc9161c2c8deebe67ffdeb4f40688fcc0
Fabien Papet <fabien@symetry.io>
2022-04-13 11:41:13 +0200

    Fix route to allow only POST requests

--- src/Resources/config/routes.php
+++ src/Resources/config/routes.php
@@ -6,6 +6,7 @@ use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 return static function (RoutingConfigurator $routingConfigurator) {
     $routingConfigurator
         ->add('stripe_webhooks', '/stripe/webhooks')
+        ->methods(['POST'])
         ->controller([WebhookController::class, '__invoke'])
     ;
 };
